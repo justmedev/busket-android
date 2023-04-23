6955daa912649f464eb71c3f2590781ee4c5bcc2
justmedev <ilja.ragnar@gmail.com>
2023-04-23 18:41:57 +0200

    add fab on home, make it possible to create list

--- app/src/main/java/dev/justme/busket/HomeFragment.kt
+++ app/src/main/java/dev/justme/busket/HomeFragment.kt
@@ -1,5 +1,6 @@
 package dev.justme.busket
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
@@ -9,10 +10,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.navigation.fragment.findNavController
 import dev.justme.busket.databinding.FragmentHomeBinding
 import dev.justme.busket.feathers.FeathersSocket
-import dev.justme.busket.feathers.responses.AuthenticationSuccessResponse
+import dev.justme.busket.feathers.responses.*
+import org.json.JSONObject
+import java.util.*
 
 
 class HomeFragment : Fragment() {
@@ -20,6 +24,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
     private var feathers: FeathersSocket? = null
     private val handler = Handler(Looper.getMainLooper())
+    private val shoppingLists = emptyList<ListOverview>().toMutableList()
 
 
     override fun onCreateView(
@@ -33,6 +38,8 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.homeListOverviewRecyclerview.adapter = EmptyAdapter();
+
         feathers = FeathersSocket.getInstance(context as Context)
         if (feathers?.user == null) {
             binding.homeMainContentContainer.visibility = View.GONE
@@ -42,6 +49,59 @@ class HomeFragment : Fragment() {
                 findNavController().navigate(R.id.action_HomeFragment_to_LoginFragment)
             })
         }
+
+        binding.createShoppingListFab.setOnClickListener(::createShoppingList)
+    }
+
+    private fun createShoppingList(view: View) {
+        val alertDialog: AlertDialog? = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater;
+            val dialogView = inflater.inflate(R.layout.dialog_create_list, null)
+
+            builder.setView(dialogView)
+                .setPositiveButton(R.string.create) { _, _ ->
+                    val createListName = dialogView.findViewById(R.id.dialog_create_list_name) as EditText
+                    val createListDescription = dialogView.findViewById(R.id.dialog_create_list_description) as EditText
+
+                    if (createListName.text.trim().length < 3) {
+                        createListName.error = "List Name has to be longer than 3 characters!"
+                        return@setPositiveButton
+                    }
+
+                    val shoppingList = ShoppingList(
+                        UUID.randomUUID().toString(),
+                        createListName.text.trim().toString(),
+                        createListDescription.text.trim().toString(),
+                        feathers?.user?.uuid ?: "UNKNOWN",
+                        ShoppingListItems(emptyList()),
+                        ShoppingListItems(emptyList()),
+                    )
+
+                    val jData = JSONObject()
+                    jData.put("listid", shoppingList.listId)
+                    jData.put("name", shoppingList.name)
+                    jData.put("description", shoppingList.description)
+                    jData.put("owner", shoppingList.owner)
+                    jData.put("entries", shoppingList.entries)
+                    jData.put("checkedEntries", shoppingList.checkedEntries)
+
+                    feathers?.service("list", FeathersSocket.Method.CREATE, jData) { data, error ->
+                        if (error != null || data == null) return@service
+                        shoppingLists.add(ListOverview(shoppingList) {
+                            Log.d("Busket ShoppingList", "pressed")
+                        })
+                        handler.post {
+                            binding.homeListOverviewRecyclerview.adapter?.notifyItemInserted(shoppingLists.lastIndex)
+                        }
+                    }
+                }
+                .setNegativeButton(R.string.cancel) { dialog, _ ->
+                    dialog.cancel()
+                }
+            builder.create()
+        }
+        alertDialog?.show()
     }
 
     private fun afterLoginSuccess(auth: AuthenticationSuccessResponse) {

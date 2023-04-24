c1c648c7eeb555035abfd2721b798ba56147f4de
justmedev <ilja.ragnar@gmail.com>
2023-04-24 18:30:19 +0200

    changed dialog to use m3 appearance

--- app/src/main/java/dev/justme/busket/HomeFragment.kt
+++ app/src/main/java/dev/justme/busket/HomeFragment.kt
@@ -1,6 +1,5 @@
 package dev.justme.busket
 
-import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
@@ -12,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dev.justme.busket.databinding.FragmentHomeBinding
 import dev.justme.busket.feathers.FeathersSocket
 import dev.justme.busket.feathers.responses.ShoppingList
@@ -50,8 +50,8 @@ class HomeFragment : Fragment() {
     }
 
     private fun createShoppingList(view: View) {
-        val alertDialog: AlertDialog? = activity?.let {
-            val builder = AlertDialog.Builder(it)
+        val alertDialog: androidx.appcompat.app.AlertDialog? = activity?.let {
+            val builder = MaterialAlertDialogBuilder(it)
             val inflater = requireActivity().layoutInflater;
             val dialogView = inflater.inflate(R.layout.dialog_create_list, null)
 

6f228ab61e3d4529c42acdd484ce9cc3e31bb290
justmedev <ilja.ragnar@gmail.com>
2023-05-02 08:18:04 +0200

    expose app resources

--- app/src/main/java/dev/justme/busket/BusketApplication.kt
+++ app/src/main/java/dev/justme/busket/BusketApplication.kt
@@ -1,11 +1,18 @@
 package dev.justme.busket
 
 import android.app.Application
+import android.content.res.Resources
 import com.google.android.material.color.DynamicColors
 
 class BusketApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
+        appResources = resources
+    }
+
+    companion object {
+        var appResources: Resources? = null
+            private set
     }
 }
